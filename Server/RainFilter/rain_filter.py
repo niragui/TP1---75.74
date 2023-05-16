2fe0b8f2d2f019a32268e2357895ebcb4c3aa4cf
niragui <ignacioiragu2@gmail.com>
2023-05-16 10:44:32 -0300

    Fix Logs

--- Server/RainFilter/rain_filter.py
+++ Server/RainFilter/rain_filter.py
@@ -17,7 +17,6 @@ class RainFilter():
         weather = self.weathers.get(weather_id)
 
         if weather is None:
-            print(f"Weather For {city} On {start} Not Found")
             return None
 
         if weather.is_rainy(PRECIPITATION_LIMIT):
