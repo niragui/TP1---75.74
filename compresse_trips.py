51216638aa25a9439f98cc1c78d56fffeb6869a0
niragui <ignacioiragu2@gmail.com>
2023-05-16 02:28:18 -0300

    Copyfile en compresser

--- compresse_trips.py
+++ compresse_trips.py
@@ -1,3 +1,5 @@
+import shutil
+
 def get_number(text):
     numero = input(text).replace(".", "").replace(",", "")
 
@@ -13,16 +15,19 @@ city = input("What City?")
 read_file = f"{city.lower()}/trips_real.csv"
 write_file = f"{city.lower()}/trips.csv"
 
-f = open(read_file, "r")
-f_write = open(write_file, "w")
+if lines > 0:
+    f = open(read_file, "r")
+    f_write = open(write_file, "w")
 
-lines_written = 0
+    lines_written = 0
 
-for line in f:
-    f_write.write(line)
-    lines_written += 1
-    if lines_written >= lines and lines > 0:
-        break
+    for line in f:
+        f_write.write(line)
+        lines_written += 1
+        if lines_written >= lines:
+            break
 
-f.close()
-f_write.close()
+    f.close()
+    f_write.close()
+else:
+    shutil.copy(read_file, write_file)
